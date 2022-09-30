aa0ecfc03fec7f2af403231ff190178807d49689
Wilman <whernandezsuesca@gmail.com>
2022-09-30 15:31:07 -0500

    Los botones Iniciar sesión, Resgitrarse y Para administradores van a sus respectivas ventanas

--- app/src/main/java/com/example/proyecto/MainActivity.kt
+++ app/src/main/java/com/example/proyecto/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.proyecto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.proyecto.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +15,37 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.inicio.setOnClickListener { inicioSesion() }
+        binding.registerUser.setOnClickListener { regitrarUser() }
+        binding.registerAdmin.setOnClickListener { registroAdmin() }
     }
+
+
+
+    fun inicioSesion(){
+        if(binding.usuario.text?.isNotEmpty() == true && binding.contrasena.text?.isNotEmpty() == true){
+            val intent = Intent(this,NavegadorUsuario::class.java)
+            startActivity(intent)
+        }else{
+            completarCampos()
+        }
+    }
+
+    fun completarCampos(){
+        Toast.makeText(this,"Llene todos los campos", Toast.LENGTH_SHORT).show()
+    }
+
+    fun regitrarUser(){
+
+        val intent = Intent(this, RegistroUsuario::class.java)
+        startActivity(intent)
+    }
+
+    fun registroAdmin(){
+
+        val intent=Intent(this, RegistroAdministrador::class.java)
+        startActivity(intent)
+    }
+
+
 }
